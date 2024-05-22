a8ce332bdc5bcd05c8d24d671a16dbe63d4d462e
unknown <diandraregita14@gmail.com>
2024-05-22 12:41:42 +0700

    resolve the CORS issue

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/user');
-const postRoutes = require('./routes/post');
-const commentRoutes = require('./routes/comment');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const userRoutes = require('./routes/user')
+const postRoutes = require('./routes/post')
+const commentRoutes = require('./routes/comment')
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const corsOptions = {
+    origin: 'https://diandrarad-bookish-space-fishstick-94rgp9qjgwv377vr-3001.app.github.dev',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corsOptions))
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
-const db = mongoose.connection;
+const db = mongoose.connection
 db.on("error", console.error.bind(console, "mongo connection error"))
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/comments', commentRoutes);
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/comments', commentRoutes)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
